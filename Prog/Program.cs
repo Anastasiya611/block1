49a31c5006e99fb6fe3182d95c1af050b852a533
Anastasiya <asa611@mail.ru>
2022-11-13 17:05:45 +0300

    сохранение программы

--- Prog/Program.cs
+++ Prog/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+string[] newarray = new string[n];
+
+for (int i= 0; i < n; i++)
+{
+    Console.Write("Введите значение: ");
+    array [i] = Console.ReadLine();
+    if (array[i].Length<4)
+    {
+        newarray[i]=array[i]; 
+    }
+}
+for (int i= 0; i < n; i++)
+{
+    if (newarray[i] != null)
+    { 
+        Console.WriteLine("[" + string.Join(", ", newarray[i]) + "]");
+    }
+}
